b62da6bc488c2900ee729efc28bc796a99d17090
Duane McDonald <mcdonald.duane13@gmail.com>
2021-03-06 15:10:24 -0500

    game logic complete
    logic working, score doesn't update

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-
-const hammer = document.querySelector('#hammer');
-const sword = document.querySelector('#sword');
-const shield = document.querySelector('#shield');
-
-
 const APP = {
     weapons: document.querySelector('.weapons'),
     prompt: document.querySelector('#prompt'),
@@ -18,24 +12,83 @@ function promptReset() {
     APP.prompt.textContent = "Select Your Weapon...";
 }
 function promptUser(e) {
-    APP.prompt.textContent = "You have selected "+e.target.title+"!";
-    setTimeout(countDown, 1600)
-    setTimeout(APP.prompt.textContent = "Computer Selected", 41000);
-    //setTimeout(computerSelect,5400);
-
+    playerChoice = e.target.title;
+    APP.prompt.textContent = "You have selected "+playerChoice+"!";
+    setTimeout(countDown, 1600, playerChoice);
 }
-function countDown () { 
+function countDown (playerChoice) {
     let seconds = 3;
     (function countdown() {
         APP.prompt.textContent = seconds
-        if(seconds -->1)
-            setTimeout(countdown, 800);
+        if(seconds -->0)
+            setTimeout(countdown, 55);
+        else updateScore(playerChoice);
     }
     )()
 }
-function computerSelect() {
-    setTimeout(APP.prompt.textContent = "Rock", 12000);
-    return;
+function updateScore(playerChoice) {
+    result = gameResult(playerChoice);
+    APP.prompt.textContent = result;
+    // let w;
+    // for (w = 0; result == "Win!"; w++) {
+    //     document.querySelector(".winScore").textContent = w;
+    //     break
+    // }
+    // let l;
+    // for (l = 0; result == "Loss!"; APP.l++) {
+    //     document.querySelector(".lossScore").textContent = l;
+    //     break
+    // }
+    // let d;
+    // for (d = 0; result == "Draw!"; APP.d++) {
+    //     document.querySelector(".drawScore").textContent = d;
+    //     break
+    // }
 }
-
+function gameResult(playerChoice) {
+    computerChoice = computerSelect();
+    setTimeout(promptReset, 4200)
+        switch (computerChoice+playerChoice) {
+            case "hammershield":
+               return "Win!"
+            case "hammersword":
+               return "Loss!"
+            case "shieldsword":
+               return "Win!"
+            case "shieldhammer":
+               return "Loss!"
+            case "swordhammer":
+               return "Win!"
+            case "swordshield":
+               return "Loss!"
+        default:
+           return "Draw!"
+        }
+    
+}
+function computerSelect () {
+    let choice = Math.floor(Math.random()* 10);
+    switch (choice) {
+        case 0:
+            return computerSelect();
+        case 1:
+            return "hammer";
+        case 2:
+            return "hammer";
+        case 3:
+            return "hammer";
+        case 4:
+            return "shield";
+        case 5:
+            return "shield";
+        case 6:
+            return "shield";
+        case 7:
+            return "sword";
+        case 8:
+            return "sword";
+        case 9:
+            return "sword";
+    }
+}
 document.addEventListener('DOMContentLoaded', APP.init());
