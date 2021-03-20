ae3559b78f39c2dd2022d337461e59c8377d5851
Duane McDonald <mcdonald.duane13@gmail.com>
2021-03-19 23:21:58 -0400

    refactor
    Corrected scoreboard update. Simplified logic for computer generated random response. Screenshot added.

--- app.js
+++ app.js
@@ -1,53 +1,59 @@
-const APP = {
-    weapons: document.querySelector('.weapons'),
-    prompt: document.querySelector('#prompt'),
-    init () {
-        promptReset();
-        APP.weapons.addEventListener("click", (e) => {
-        promptUser(e);
-        })
-    }
+const weapons = document.querySelector('.weapons');
+const prompt = document.querySelector('#prompt');
+let win = 0;
+let loss = 0;
+let draw = 0;
+
+function init () {
+    promptReset();
+    weapons.addEventListener("click", (e) => {
+    promptUser(e);
+    })
 }
+
 function promptReset() {
-    APP.prompt.textContent = "Select Your Weapon...";
+    prompt.textContent = "Select Your Weapon...";
 }
+
 function promptUser(e) {
     playerChoice = e.target.title;
-    APP.prompt.textContent = "You have selected "+playerChoice+"!";
+    prompt.textContent = "You have selected "+playerChoice+"!";
     setTimeout(countDown, 1600, playerChoice);
 }
+
 function countDown (playerChoice) {
     let seconds = 3;
     (function countdown() {
-        APP.prompt.textContent = seconds
+        prompt.textContent = seconds
         if(seconds -->0)
             setTimeout(countdown, 500);
         else updateScore(playerChoice);
     }
     )()
 }
+
 function updateScore(playerChoice) {
     result = gameResult(playerChoice);
-    APP.prompt.textContent = result;
-    // let w;
-    // for (w = 0; result == "Win!"; w++) {
-    //     document.querySelector(".winScore").textContent = w;
-    //     break
-    // }
-    // let l;
-    // for (l = 0; result == "Loss!"; APP.l++) {
-    //     document.querySelector(".lossScore").textContent = l;
-    //     break
-    // }
-    // let d;
-    // for (d = 0; result == "Draw!"; APP.d++) {
-    //     document.querySelector(".drawScore").textContent = d;
-    //     break
-    // }
+    prompt.textContent = result;
+    switch (result) {
+        case "Win!":
+            win++;
+            document.querySelector('#winScore').textContent = `${win}`;
+        break; 
+        case "Loss!":
+            loss++;
+            document.querySelector('#lossScore').textContent = `${loss}`;
+        break;
+        case "Draw!":
+            draw++;
+            document.querySelector('#drawScore').textContent = `${draw}`; 
+        break;
+    }
 }
+
 function gameResult(playerChoice) {
-    computerChoice = computerSelect();
-    setTimeout(promptReset, 6200)
+    const computerChoice = computerSelect();
+    setTimeout(promptReset, 4300)
         switch (computerChoice+playerChoice) {
             case "hammershield":
                return "Win!"
@@ -64,31 +70,11 @@ function gameResult(playerChoice) {
         default:
            return "Draw!"
         }
-    
 }
+
 function computerSelect () {
-    let choice = Math.floor(Math.random()* 10);
-    switch (choice) {
-        case 0:
-            return computerSelect();
-        case 1:
-            return "hammer";
-        case 2:
-            return "hammer";
-        case 3:
-            return "hammer";
-        case 4:
-            return "shield";
-        case 5:
-            return "shield";
-        case 6:
-            return "shield";
-        case 7:
-            return "sword";
-        case 8:
-            return "sword";
-        case 9:
-            return "sword";
-    }
+    const gameWeapons = ['hammer', 'shield', 'sword']
+    return gameWeapons[Math.floor(Math.random() * gameWeapons.length)];
 }
-document.addEventListener('DOMContentLoaded', APP.init());
+
+document.addEventListener('DOMContentLoaded', init());
